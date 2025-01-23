d242c1a239ee2b99ed2d07804460d2ca3b8e2c17
seongmee <chaewon.ryu24@gmail.com>
2025-01-23 13:14:56 +0900

    메인프로젝트

---
+++ MainProject/NouvoProject/src/test/java/com/himedia/nouvoproject/NouvoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himedia.nouvoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NouvoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
